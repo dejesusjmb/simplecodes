dbf32fe2125bdd019cb595a3aab4781de75f16ab
JM De Jesus <josemaribdejesus@gmail.com>
2017-02-27 02:22:57 +0800

    committed multiplication table without any loops

---
+++ multiplicationtable.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int printRow(int count, int max)
+{
+    if (max==0)
+        return 0;
+    int x = printRow(count, --max) + count;
+    std::cout << x << '\t';
+    return x;
+}
+
+int printTable(int row, int col)
+{
+    if (row==0)
+        return 1;
+    int count = printTable(--row, col);
+    int temp = printRow(count, col);
+    std::cout << std::endl;
+    return ++count;
+}
+
+int main()
+{
+    int row, col;
+    std::cout << "Input row: ";
+    std::cin >> row;
+    std::cout << "Input column: ";
+    std::cin >> col;
+
+    printTable(row, col);
+    return 0;
+}
